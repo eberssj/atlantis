50c8c7cfac7b2355759c9bb736a16ddd41b6005b
Eber Junior <eberssjunior12@gmail.com>
2024-11-01 08:54:38 -0300

    começando a página home

--- lista 4/atlantis-waterpark/src/components/Home/Home.tsx
+++ lista 4/atlantis-waterpark/src/components/Home/Home.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import retangulo from '../../assets/img/retangulo.png'; 
+import '../Home/Home.css';
 
 const Home: React.FC = () => {
   return (
     <div className="home">
-      <h2>Seja bem-vindo ao sistema Atlantis</h2>
+      <img src={retangulo} alt="Banner Topo" className="banner-topo" />
+      <div className="conteudo">
+      <h2>Seja bem-vindo ao sistema <span className="atlantis-text">Atlantis</span></h2>
+        <p>Aqui está o painel de controle, caso queira acessar outras partes do sistema, use o menu ao lado.</p>
+        <p>Em caso de dúvidas, entre em contato conosco! Acesse:</p>
+        <ul>
+          <li>📞 Central de atendimento: +55 (12) 98222222222</li>
+          <li>📧 Email: <a href="mailto:atlantis@ebermail.com">atlantis@ebermail.com</a></li>
+          <li>💬 ChatBot: <a href="https://support.atlantis.com/pt-br/contact-us" target="_blank">https://support.atlantis.com/pt-br/contact-us</a></li>
+        </ul>
+      </div>
     </div>
   );
 };
