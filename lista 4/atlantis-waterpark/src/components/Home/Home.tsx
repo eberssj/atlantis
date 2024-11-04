ef903c0003dc748f43b213689524939612e7e400
Eber Junior <eberssjunior12@gmail.com>
2024-11-04 08:39:31 -0300

    ajustando pagina home

--- lista 4/atlantis-waterpark/src/components/Home/Home.tsx
+++ lista 4/atlantis-waterpark/src/components/Home/Home.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import retangulo from '../../assets/img/retangulo.png'; 
+import telefoneIcon from '../../assets/img/telefone.png'; // Imagem do ícone de telefone
+import emailIcon from '../../assets/img/email.png';       // Imagem do ícone de email
+import chatbotIcon from '../../assets/img/chatbot.png';   // Imagem do ícone de chatbot
 import '../Home/Home.css';
 
 const Home: React.FC = () => {
@@ -7,13 +10,31 @@ const Home: React.FC = () => {
     <div className="home">
       <img src={retangulo} alt="Banner Topo" className="banner-topo" />
       <div className="conteudo">
-      <h2>Seja bem-vindo ao sistema <span className="atlantis-text">Atlantis</span></h2>
-        <p>Aqui está o painel de controle, caso queira acessar outras partes do sistema, use o menu ao lado.</p>
-        <p>Em caso de dúvidas, entre em contato conosco! Acesse:</p>
+        <h2>Seja bem-vindo ao sistema <span className="atlantis-text">Atlantis</span></h2>
+        <p className="descricao">Aqui está o painel de controle, caso queira acessar outras partes do sistema, use o menu ao lado.</p>
+        <p className="descricao">Em caso de dúvidas, entre em contato conosco! Acesse:</p>
         <ul>
-          <li>📞 Central de atendimento: +55 (12) 98222222222</li>
-          <li>📧 Email: <a href="mailto:atlantis@ebermail.com">atlantis@ebermail.com</a></li>
-          <li>💬 ChatBot: <a href="https://support.atlantis.com/pt-br/contact-us" target="_blank">https://support.atlantis.com/pt-br/contact-us</a></li>
+          <li>
+            <img src={telefoneIcon} alt="Telefone" className="icon" />
+            <div className="info-text">
+              <span className="titulo">Central de atendimento:</span>
+              <a href="tel:+55129822222222" className="contato">+55 (12) 98222222222</a>
+            </div>
+          </li>
+          <li>
+            <img src={emailIcon} alt="Email" className="icon" />
+            <div className="info-text">
+              <span className="titulo">Email:</span>
+              <a href="mailto:atlantis@ebermail.com" className="contato">atlantis@ebermail.com</a>
+            </div>
+          </li>
+          <li>
+            <img src={chatbotIcon} alt="ChatBot" className="icon" />
+            <div className="info-text">
+              <span className="titulo">ChatBot:</span>
+              <a href="https://support.atlantis.com/pt-br/contact-us" target="_blank" className="contato">https://support.atlantis.com/pt-br/contact-us</a>
+            </div>
+          </li>
         </ul>
       </div>
     </div>
